d0c0bde4ebfb2e87c392f1b9adcdfbb9b5ba6792
vassildinev <vassildinev@gmail.com>
2015-08-29 12:37:24 +0300

    App skeleton pretty much built

--- js/app.js
+++ js/app.js
@@ -2,11 +2,32 @@ import $ from './lib/jquery.js';
 import sammy from './lib/sammy.js';
 
 (function () {
+    var wrapper = $('#main-content');
     var app = sammy("#main-content", function () {
-        this.get("#/", function () {
-            $('body').append($('<div/>').html('Pesho'));
+        this.get('#/home', function () {
+            this.load('./templates/home.html', function (data) {
+                wrapper.html(data);
+            });
+        });
+
+        this.get("#/log-in", function () {
+            this.load('./templates/log-in.html', function (data) {
+                wrapper.html(data);
+            });
+        });
+
+        this.get("#/sign-up", function () {
+            this.load('./templates/sign-up.html', function (data) {
+                wrapper.html(data);
+            });
+        });
+
+        this.get("#/about", function () {
+            this.load('./templates/about.html', function (data) {
+                wrapper.html(data);
+            });
         });
     });
 
-    app.run("#/");
+    app.run("#/home");
 }());
