198c5eda0f16da129ae19619e94a7a90f22e67b6
Bianca Hinova <bianca.hinova@gmail.com>
2015-09-03 02:40:33 +0300

    userGames can split by myTurn

--- js/controllers/Games.js
+++ js/controllers/Games.js
@@ -4,16 +4,28 @@ function showGames() {
         $('div .myGamesWrapper').html('You must be logged in to see this page');
     }
 
-    var currentUserImagesToGuess = Parse.User.current().get('games');
-    var games = {};
-    currentUserImagesToGuess.forEach(function (game) {
+    var currentUserGames = Parse.User.current().get('games');
+    var games = {}; // is this used anywhere?
+    var myTurns = [];
+    var theirTurns = [];
+    currentUserGames.forEach(function (game) {
         var user = game.player === Parse.User.current().get('username') ? game.enemy : game.player;
         games[user] = game.images;
+        if(game.myTurn){
+            myTurns.push(game)
+        }
+        else{
+            theirTurns.push(game);
+        }
     });
+
+
+
+
 
     localStorage.setItem(currentUser.get('username'), JSON.stringify(games));
 
-    if (currentUserImagesToGuess.length === constants.initialStateOfGuess.initial) {
+    if (currentUserGames.length === constants.initialStateOfGuess.initial) {
         var noFriends = 'You have no games. Start new game!'; ///???
         $('#myGamesWrapper').html(noFriends);
         $('#my-turn').hide();
@@ -22,12 +34,12 @@ function showGames() {
         //TODO: eventually boolean to check turns & split to my turn & their turn
         var template = $('#my-games-template').html();
         var compiledTemplate = Handlebars.compile(template);
-        var html = compiledTemplate({
-            games: currentUserImagesToGuess
+        var turnsTemplate = compiledTemplate({
+            myTurnGames: myTurns,
+            theirTurnGames: theirTurns
         });
 
-        $('#my-turn').append(html);
-        $('#their-turn').hide();
+        $('#main-content').html(turnsTemplate);
 
     }
 }
