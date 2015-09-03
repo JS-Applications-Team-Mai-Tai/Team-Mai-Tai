3d9612aa8ba2eaf798f7de17ef70f6ef9cd7392c
VenelinGP <venelin.petkov@netcoms.eu>
2015-09-03 19:33:28 +0300

    Added more tests

--- tests/game_test.js
+++ tests/game_test.js
@@ -4,14 +4,23 @@ describe("Game", function() {
     describe("constructor", function () {
         it("should create a game 'classic' with mode = 'classic'", function () {
             var game = new Game('Ivan', 'Vankata','classic');
-            console.log(game);
             expect(game.mode).to.equal("classic");
         });
 
         it("should create a game 'classic' with mode = ' '", function () {
             var game = new Game('Ivan', 'Vankata','');
-            console.log(game);
             expect(game.mode).to.equal("classic");
+        });
+
+        it("should return a empty array of images", function () {
+            var game = new Game('Ivan', 'Vankata','');
+            expect(game.images.length).to.equal(0);
+        });
+
+        it("should return a true game.myTurn", function () {
+            var game = new Game('Ivan', 'Vankata','');
+            console.log(game);
+            expect(game.myTurn).to.equal(true);
         });
     });
 });
