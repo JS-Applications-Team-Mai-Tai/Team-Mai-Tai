134565c248d9125b6e08b330c2ace85e249c6b2b
Bianca Hinova <bianca.hinova@gmail.com>
2015-09-03 23:31:13 +0300

    imgs for coins&diamonds in Profile

--- js/controllers/UserProfileController.js
+++ js/controllers/UserProfileController.js
@@ -2,7 +2,8 @@ import {UserProfile} from 'js/whereMagicHappens/userProfile.js';
 
 function visualizeProfile(){
     var currentUser = Parse.User.current();
-    var userProfile = new UserProfile(currentUser);
+    var currentUsername = Parse.User.current().get('username');
+    var userProfile = new UserProfile(currentUsername);
     userProfile.visualize();
 }
 
